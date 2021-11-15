c942ef041b7b1997434e872bf9d10c77e7b21f5d
Alex Stetsenko <jazzlevit@gmail.com>
2021-11-15 23:15:50 +0200

    VanillaJS solution

--- script.js
+++ script.js
@@ -10,16 +10,32 @@
 
 // Jquery
 
-$('.bi').click(function (event) {
-
-    // change input type
-    const input = $(this).parent().find('input');
-    const currentType = input.attr('type');
-
-    input.attr('type', currentType === 'text' ? 'password' : 'text');
-
-    // Change icon style
-    $(this).toggleClass('bi-eye-slash-fill')
-});
+// $('.bi').click(function (event) {
+//
+//     // change input type
+//     const input = $(this).parent().find('input');
+//     const currentType = input.attr('type');
+//
+//     input.attr('type', currentType === 'text' ? 'password' : 'text');
+//
+//     // Change icon style
+//     $(this).toggleClass('bi-eye-slash-fill')
+// });
 
 // VanillaJs
+
+const icons = [...document.getElementsByClassName('bi')];
+
+icons.forEach(function(element) {
+
+    element.addEventListener('click', function (event) {
+        // change input type
+        const input = this.parentElement.querySelector('input');
+        const inputCurrentType = input.getAttribute('type');
+
+        input.setAttribute('type', inputCurrentType === 'text' ? 'password' : 'text');
+
+        // Change icon style
+        this.classList.toggle('bi-eye-slash-fill');
+    });
+})
