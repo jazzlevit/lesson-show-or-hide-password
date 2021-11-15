55338afad2d5b0f5655e9cab5c796e9517d36c27
Alex Stetsenko <jazzlevit@gmail.com>
2021-11-15 21:57:41 +0200

    Init lesson

---
+++ script.js
@@ -0,0 +1,13 @@
+/**
+ * Please finish the lesson by adding needed js:
+ * - You can write code with jquery OR VanillaJs
+ * - but better to try it both, two variants are recommended for learning
+ *
+ * Icon classes:
+ * - bi-eye-fill
+ * - bi-eye-slash-fill
+*/
+
+// Jquery
+
+// VanillaJs
